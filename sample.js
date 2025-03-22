aa28ff9e60e4530cd7bafc3365cd49057a425f60
elias <eltes2010@gmail.com>
2025-03-22 13:44:14 +0100

    added a simple calculator

--- sample.js
+++ sample.js
@@ -5,11 +5,30 @@ let pricOfTheFood = 20
 
 let quantity = 3
 let totalPrice = pricOfTheFood * quantity
-let isTheFoodtypeTibs=false
+let isTheFoodtypeTibs = false
 
-if(isTheFoodtypeTibs){
-   totalPrice *=3
+if (isTheFoodtypeTibs) {
+   totalPrice *= 3
 }
 
-console.log(`new food order: ${quantity} ${isTheFoodtypeTibs?"Tibs":"Normal food"} the price is $${pricOfTheFood} and the tottal price is $${totalPrice}`)
+console.log(`new food order: ${quantity} ${isTheFoodtypeTibs ? "Tibs" : "Normal food"} the price is $${pricOfTheFood} and the tottal price is $${totalPrice}`)
 
+// simple calculator
+
+let a = 12
+let b = 13
+let command = "*"
+let c;
+if (command === "+") {
+   c = a + b
+} else if (command === "-") {
+   c = a - b
+} else if (command === "*") {
+   c = a * b
+} else if (command === "/") {
+   c = a / b
+} else {
+   console.log("invalid command")
+}
+
+console.log(`the result is ${c}`)
